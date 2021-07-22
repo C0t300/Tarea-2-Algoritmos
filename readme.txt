cab485df5a6986190706ad669069bc2eb8997058
Jose Runin <cote.runin@gmail.com>
2021-07-21 23:20:10 -0400

    commit 1 asies

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Quickselect
+asies
---
+++ tarea.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int particion(int arr[], int l, int r)
+{
+	int x = arr[r], i = l;
+	for (int j = l; j <= r - 1; j++) {
+		if (arr[j] <= x) {
+			swap(arr[i], arr[j]);
+			i++;
+		}
+	}
+	swap(arr[i], arr[r]);
+	return i;
+}
+
+
+
+int quickselect(int arr[], int l, int r, int k)
+{
+
+	if (k > 0 && k <= r - l + 1) {
+
+		int index = particion(arr, l, r);
+
+		if (index - l == k - 1)
+			return arr[index];
+
+		
+		if (index - l > k - 1)
+			return quickselect(arr, l, index - 1, k);
+
+		return quickselect(arr, index + 1, r, k - index + l - 1);
+	}
+
+	return INT_MAX;
+}
+
+int main(){
+    int left;
+    int right;
+    while (cin >> left >> right){ // left cantidad de casos y right salidas requeridas
+        int numeros[left];
+        string nombres[left];
+        int buffer[left];
+        for (int i = 0; i < left; i++){
+            cin >> numeros[i] >> nombres[i];
+        }
+        for (int i = 0; i < left; i++){
+            buffer[i] = numeros[i];
+        }
+        for(int i = 0; i < right; i++){
+
+            int retorno = quickselect(numeros, 0, left, left-i);
+            for(int i = 0; i < left; i++){
+                if (buffer[i] == retorno){
+                    cout << buffer[i] << " " << nombres[i] << endl;
+                }
+            }
+            // cout << numeros[pos] << endl << nombres[pos] << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
